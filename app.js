31ae6b3a1af3d39fc28a404bab83c7af7aea7d45
itisaby <arnabbibhuti01@gmail.com>
2022-04-23 21:33:20 +0530

    Handling errors and improving the project setup

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
+const order = require('./api/routes/orders');
 
+//Routes handling all the requests
 app.use("/products", productRoutes);
-
+app.use("/order", order);
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app;
