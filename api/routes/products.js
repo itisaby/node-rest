29151d1f42b4a2375fd210f03749826557552d1c
itisaby <arnabbibhuti01@gmail.com>
2022-05-03 11:58:46 +0530

    Patch Delete POST GET request connected to MONGODB

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,48 +2,124 @@ const express = require('express');
 const routes = express.Router();
 const bodyParser = require('body-parser');
 const Product = require('../Models/products');
+const mongoose = require('mongoose');
 var jsonParser = bodyParser.json()
 
 routes.get('/', (req, res, next) => {
-    res.status(200).json({
-          message: 'Handling Get requests /products'
-    });
+    // res.status(200).json({
+    //     message: 'Handling Get requests /products'
+    // });
+    Product.find()
+        .exec()
+        .then(docs => {
+            res.status(200).json(docs);
+            console.log(docs);
+        }).catch(err => {
+            res.status(500).json({
+                error: err
+            });
+            console.log(err);
+        });
 })
 
-routes.post('/', jsonParser,(req, res, next) => {
-    const product = {
+routes.post('/', jsonParser, (req, res, next) => {
+    const product = new Product({
+        _id: mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(201).json({
-          message: 'Handling Post requests /products',
-            createdProduct: product
-    });
-})
-routes.patch('/', (req, res, next) => {
-    res.status(200).json({
-          message: 'Handling Patch requests /products'
-    });
-})
-routes.delete('/', (req, res, next) => {
-    res.status(200).json({
-          message: 'Handling Delete requests /products'
-    });
+    })
+    product.save().then(result => {
+        // res.status(201).json({
+        //     message: 'Handling Post requests /products',
+        //     createdProduct: Product
+        // });
+        console.log(result);
+        res.status(201).json({
+            message: 'Handling Post requests /products',
+            createdProduct: result
+        });
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
+        console.log(error);
+    })
+
 })
 
-routes.post("/:productId", (req, res, next) => {
+routes.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if(id==="special"){
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        });
-    } else{
-        res.status(200).json({
-            message: 'Post request for product id ',
-            id: id
-        });
-    }
+    // const updateOps = {};
+    // for (const ops of req.body) {
+    //     updateOps[ops.propName] = ops.value;
+    //   }
+    
+    Product.updateMany({ _id: id }, { $set: { name: req.body.name, price: req.body.price } })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+    // res.status(200).json({
+    //     message: 'Handling Patch requests /products'
+    // });
+})
+
+
+routes.delete('/:productId', (req, res, next) => {
+    // res.status(200).json({
+    //     message: 'Handling Delete requests /products'
+    // });
+    const id = req.params.productId;
+    Product.remove({
+        _id: id
+    }).exec()
+        .then(result => {
+            res.status(200).json({
+                result
+            });
+        }).catch(error => {
+            res.status(500).json({
+                error: error
+            });
+            console.log(error);
+        })
+})
+
+routes.get("/:productId", (req, res, next) => {
+    const id = req.params.productId;
+    // if(id==="special"){
+    //     res.status(200).json({
+    //         message: 'You discovered the special ID',
+    //         id: id
+    //     });
+    // } else{
+    //     res.status(200).json({
+    //         message: 'Post request for product id ',
+    //         id: id
+    //     });
+    // }
+    Product.findById(id)
+        .exec()
+        .then(product => {
+            if (product) {
+                res.status(200).json(product);
+            } else {
+                res.status(404).json({
+                    message: 'No valid entry found for provided ID'
+                });
+            }
+        }).catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        })
 })
 
 module.exports = routes;
--- app.js
+++ app.js
@@ -3,28 +3,36 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const productRoutes = require('./api/routes/products');
 const order = require('./api/routes/orders');
 
 const username = "node-rest";
-const password = "noderest";
+const password = process.env.PASSWORD;
 const cluster = "Cluster1";
 const dbname = "NODE-REST";
 
 mongoose.connect(
-  `mongodb+srv://node-rest:${password}@cluster1.fstlx.mongodb.net/nodedb?retryWrites=true&w=majority`, 
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
+    `mongodb+srv://node-rest:${password}@cluster1.fstlx.mongodb.net/nodedb?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(() => {
+    console.log('Connected to database');
+})
+    .catch(() => {
+        console.log('Connection failed');
+    }
+    );
 // Routes handling all the requests
 app.use("/products", productRoutes);
 app.use("/order", order);
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ 
+app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
@@ -32,7 +40,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if(req.method === 'OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
@@ -44,7 +52,7 @@ app.use((req, res, next) => {
     error.status = 404;
     next(error);
 })
-app.use((error, req, res, next) =>{
+app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {
