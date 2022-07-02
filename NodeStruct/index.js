6b4af9348ee1fe3dc73bd71c60fbfc06c974c685
itisaby <arnabbibhuti01@gmail.com>
2022-07-02 20:06:58 +0530

    Express added

--- NodeStruct/index.js
+++ NodeStruct/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 8000;
+
+app.get("/", (req, res)=>{
+    res.send("Hello World");
+})
+
 console.log('test')
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
